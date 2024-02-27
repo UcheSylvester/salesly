661754c4a22b5e684b6b66d9c3ae39adefed42e4
UcheSylvester <okorocode@gmail.com>
2024-02-27 07:54:59 +0100

    chores: add actions row to table

--- src/features/SalesInventory/containers/SalesInventoryList.tsx
+++ src/features/SalesInventory/containers/SalesInventoryList.tsx
@@ -1,37 +1,95 @@
 import { useReplicache } from "@/providers/ReplicacheProvider"
-import { Container, List, Stack, Text, Title } from "@mantine/core"
+import { ActionIcon, Badge, Button, Container, Group, List, Menu, Stack, Text, Title } from "@mantine/core"
 import { useSubscribe } from "replicache-react"
-import { listSalesInventories } from "../helpers"
+import { SalesInventory, listSalesInventories } from "../helpers"
 import { useRouter } from "next/router"
+import { MRT_ColumnDef, MantineReactTable, useMantineReactTable } from "mantine-react-table"
+import { useMemo } from "react"
+import { IconDotsVertical } from "@tabler/icons-react"
 
 export const SalesInventoryList = () => {
   const router = useRouter()
   const r = useReplicache()
   const sales = useSubscribe(r, listSalesInventories, {default: []})
+  const columns = useReferralListTableColumn()
+
+  const table = useMantineReactTable({
+    columns,
+    data: sales || [],
+    enablePagination: false,
+    enableTableFooter: false,
+    enableBottomToolbar: false,
+    enableColumnActions: false,
+    enableColumnOrdering: false,
+    enableTopToolbar: false,
+    positionActionsColumn: 'last',
+    enableRowActions: true,
+    renderRowActions: ({ row }) => (
+      <Menu position="bottom-end" shadow={'md'} withinPortal={true}>
+        <Menu.Target>
+          <ActionIcon radius={'sm'} variant="default" ml={'auto'}>
+            <IconDotsVertical height={18} width={18} />
+          </ActionIcon>
+        </Menu.Target>
+
+        <Menu.Dropdown>
+          <Menu.Item onClick={() => router.push(`/inventory/${row.original.id}`)}>
+            Edit
+          </Menu.Item>
+          <Menu.Item onClick={() => r?.mutate.deleteInventory(row.original.id)}>
+            Delete
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+    ),
+  })
 
   return (
-    <Container>
-      <Stack spacing="xl">
-        <Title>Sales Inventory List</Title>
-        <List spacing={10}>
-          {
-            sales.map(sale => (
-              <List.Item key={sale.id} onClick={() => router.push(`/inventory/${sale.id}`)} sx={{
-                height: '50px',
-                borderBottom: `1px solid #e1e1e1`,
-                display: 'flex',
-                alignItems: 'center',
-                cursor: 'pointer'
-              }}>
-                <Text>
-                  {sale.product} - {sale.customer} - {sale.quantity} - ${sale.price} - ${sale.amountPaid} - {sale.status}
-                </Text>
-              </List.Item>
-            ))
-          }
+    <Container size='lg'>
+      <Stack spacing={50}>
+        <Group position="apart">
+          <Title>Sales Inventory</Title>
+          <Button onClick={() => router.push('/create')}>Create</Button>
+        </Group>
 
-        </List>
+        <MantineReactTable table={table} />
       </Stack>
     </Container>
   )
 }
+
+
+export const useReferralListTableColumn = () => {
+  return useMemo<MRT_ColumnDef<SalesInventory>[]>(
+    () => [
+      {
+        accessorKey: 'product',
+        header: 'Product',
+      },
+      {
+        accessorKey: 'customer',
+        header: 'Customer',
+      },
+      {
+        id: 'quantity',
+        header: 'Quantity',
+        accessorFn: ({ quantity }) => quantity ? `$${quantity}` : <>--</>,
+
+      },
+      {
+        accessorKey: 'amountPaid',
+        header: 'Amount Paid',
+      },
+      {
+        id: 'status',
+        header: 'Status',
+        accessorFn: (props) => (
+          <Badge>
+            {props.status}
+          </Badge>
+        ),
+      },
+    ],
+    []
+  );
+};
